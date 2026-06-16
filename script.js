4eb255214b5ecab52bbf35f7cfe7c73e95e81ffa
Arsenii Hrokh <a.m.groh07@gmail.com>
2026-06-16 13:43:36 +0300

    feat: remove useless table features

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
 const pivot = new WebDataRocks({
     container: "#pivotContainer",
     toolbar: false,
+    options: {
+        grid: {
+            showGrandTotals: "none",
+            showTotals: "none",
+            showFilter: false,
+        }
+    }
   });
